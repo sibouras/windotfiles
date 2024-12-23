dca2bb55876487342339e42782718aedd7fcf9d7
ala <marzoukala2018@gmail.com>
2024-12-23 12:39:24 +0100

    yazi: Folder-specific rules, show modified time on the status line

--- AppData/Roaming/yazi/config/init.lua
+++ AppData/Roaming/yazi/config/init.lua
@@ -1,1 +1,12 @@
 require("git"):setup()
+require("folder-rules"):setup()
+
+-- Show modified time of the file under the cursor on the status line
+Status:children_add(function()
+  local h = cx.active.current.hovered
+  return ui.Line {
+    -- ui.Span(os.date("%y-%m-%d %H:%M ", h.cha.mtime // 1)):fg("blue"),
+    ui.Span(os.date(_, tostring(h.cha.mtime):sub(1, 10))):fg("blue"),
+    ui.Span(" "),
+  }
+end, 500, Status.RIGHT)
